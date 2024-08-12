f039e1fbd7a941b899d8b615779abd0bb7a9ffea
LuchoProgramer <luchoviteri1990@gmail.com>
2024-08-12 12:39:19 -0500

    yaman

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    // Manejar el botón de agendar cita
+    // Cachear elementos del DOM
     const appointmentForm = document.getElementById('appointmentForm');
+    const contactForm = document.getElementById('contact-form');
+    const backToTop = document.getElementById('backToTop');
+    
+    // Manejar el botón de agendar cita
     if (appointmentForm) {
         const submitButton = appointmentForm.querySelector('button[type="button"]');
         if (submitButton) {
@@ -10,14 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Mostrar u ocultar el botón de regreso al principio en función del desplazamiento
-    window.addEventListener('scroll', function() {
-        const backToTop = document.getElementById('backToTop');
-        if (window.scrollY > 300) { // Mostrar el botón si se desplaza más de 300 píxeles hacia abajo
-            backToTop.style.display = 'block';
-        } else {
-            backToTop.style.display = 'none';
-        }
-    });
+    if (backToTop) {
+        window.addEventListener('scroll', () => {
+            backToTop.style.display = window.scrollY > 300 ? 'block' : 'none';
+        });
+    }
+
+    // Manejar el formulario de contacto
+    if (contactForm) {
+        contactForm.addEventListener('submit', handleContactFormSubmit);
+    }
 });
 
 function prepareWhatsAppMessage() {
@@ -31,8 +36,39 @@ function prepareWhatsAppMessage() {
     }
 
     const message = `Nombre de la Mascota: ${encodeURIComponent(nombreMascota)}%0AFecha: ${encodeURIComponent(fecha)}%0AHora: ${encodeURIComponent(hora)}`;
-    const whatsappUrl = `https://wa.me/593987005084?text=${message}`;
-
-    window.location.href = whatsappUrl;
+    window.location.href = `https://wa.me/593987005084?text=${message}`;
     return false; // Previene el envío del formulario tradicional
+}
+
+function handleContactFormSubmit(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+    event.stopPropagation(); // Prevenir que el evento se propague
+
+    const form = event.target;
+
+    // Validar el formulario
+    if (!form.checkValidity()) {
+        form.classList.add('was-validated');
+        return;
+    }
+
+    // Verificar reCAPTCHA
+    const recaptchaResponse = grecaptcha.getResponse();
+    if (!recaptchaResponse) {
+        alert('Por favor, verifica el reCAPTCHA.');
+        return;
+    }
+
+    // Enviar el formulario usando EmailJS
+    emailjs.sendForm('service_066w7i9', 'template_q7peraq', form, 'jgAwVR-flCTKQnK_X')
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('¡El mensaje ha sido enviado correctamente!');
+            form.reset(); // Reiniciar el formulario
+            form.classList.remove('was-validated'); // Remover la clase de validación
+            grecaptcha.reset(); // Reiniciar reCAPTCHA
+        }, function(error) {
+            console.error('FAILED...', error);
+            alert('Hubo un problema al enviar el mensaje.');
+        });
 }
